af13cc6ccf285be177ab5d86d1a21e9ea0f6af11
Jalaladdin <jalal.rafiyev@gmail.com>
2024-02-27 21:31:36 +0400

    day2.3

--- app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -9,7 +9,7 @@ import {
 } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
 
-import { CategoryColumn } from './columns';
+import { BillboardColumn } from './columns';
 import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
 import toast from 'react-hot-toast';
 import { useParams, useRouter } from 'next/navigation';
@@ -18,7 +18,7 @@ import axios from 'axios';
 import { AlertModal } from '@/components/modals/alert-modal';
 
 interface CellActionProps {
-	data: CategoryColumn;
+	data: BillboardColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({ data }) => {
@@ -30,17 +30,17 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 
 	const onCopy = (id: string) => {
 		navigator.clipboard.writeText(id);
-		toast.success('Category ID copied to the clipboard');
+		toast.success('Billboard ID copied to the clipboard');
 	};
 
 	const onDelete = async () => {
 		try {
 			setLoading(true);
-			await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
+			await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
 			router.refresh();
-			toast.success('Category deleted');
+			toast.success('Billboard deleted');
 		} catch (error) {
-			toast.error('Make sure you removed all products using this category first.');
+			toast.error('Make sure you removed all categories using this billboard first.');
 		} finally {
 			setLoading(false);
 			setOpen(false);
@@ -70,7 +70,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
 						<Copy className='mr-2 h-4 w-4' />
 						Copy ID
 					</DropdownMenuItem>
-					<DropdownMenuItem onClick={() => router.push(`/${params.storeId}/categories/${data.id}`)}>
+					<DropdownMenuItem onClick={() => router.push(`/${params.storeId}/billboards/${data.id}`)}>
 						<Edit className='mr-2 h-4 w-4' />
 						Update
 					</DropdownMenuItem>
